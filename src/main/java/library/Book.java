17d241129854419824c7874e48bad7b379eb892d
Mihaly Toth <Mihaly.Toth@kuka.com>
2021-08-03 20:41:33 +0200

    Entity is created

---
+++ src/main/java/library/Book.java
@@ -0,0 +1,31 @@
+package library;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "books")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String author;
+    private String title;
+
+    @Column(name = "rental_date")
+    private String rentalDate;
+
+    public Book(String author, String title, String rentalDate) {
+        this.author = author;
+        this.title = title;
+        this.rentalDate = rentalDate;
+    }
+}
